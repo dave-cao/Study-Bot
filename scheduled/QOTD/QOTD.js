05be32c9990f3c7c94b1d3f7fdcdd018b218479f
David Cao <dacao@protonmail.com>
2022-01-31 11:42:30 -0700

    made QOTD bot display random

--- scheduled/QOTD/QOTD.js
+++ scheduled/QOTD/QOTD.js
@@ -9,8 +9,11 @@ const fs = require('fs');
 const config = require('../../config.json');
 
 const saveFileName = '/home/milk/personalBot/Personal-Bot/scheduled/QOTD/QOTDData.json';
-const quoteFile = '/home/milk/personalBot/Personal-Bot/scheduled/QOTD/combined_quotes.txt';
-const questionFile = '/home/milk/personalBot/Personal-Bot/scheduled/QOTD/raw_questions.txt';
+const quoteFile = '/home/milk/personalBot/Personal-Bot/scheduled/QOTD/quotes.txt';
+const questionFile = '/home/milk/personalBot/Personal-Bot/scheduled/QOTD/questions.txt';
+// const saveFileName = 'QOTDData.json';
+// const quoteFile = 'quotes.txt';
+// const questionFile = 'questions.txt';
 
 const saveData = (userData) => {
   // Saves current userData to JSON file
@@ -28,11 +31,20 @@ client.login(config.token);
 client.once('ready', () => {
   console.log('QOTD Scheduler is ready');
   const guild = client.guilds.cache.get('787354978166898708');
-  const channel = client.channels.cache.get('813852697462833173');
+  const channel = client.channels.cache.get('813852697462833173'); // real
+  // const channel = client.channels.cache.get('793302938453803008'); // test
+
+  // Grab from text files
+  const textQuotes = fs
+    .readFileSync(quoteFile, 'utf8')
+    .split('\n')
+    .filter((quote) => quote);
+  const textQuestions = fs
+    .readFileSync(questionFile, 'utf8')
+    .split('\n')
+    .filter((question) => question);
 
   if (guild && channel) {
-    // Display a question of the day
-    // Increase JSON by 1
     let QOTDData = {};
     // Read JSON file
     if (fs.existsSync(saveFileName)) {
@@ -40,37 +52,33 @@ client.once('ready', () => {
       QOTDData = JSON.parse(dataString);
     } else {
       // if file doesn't exist, then make default from this
-      const textQuotes = fs.readFileSync(quoteFile, 'utf8').split('\n');
-      const textQuestions = fs.readFileSync(questionFile, 'utf8').split('\n');
       QOTDData = {
-        quoteNumber: 0,
-        questionNumber: 0,
+        quoteNumber: Math.floor(Math.random() * textQuotes.length),
+        questionNumber: Math.floor(Math.random() * textQuestions.length),
         quotes: textQuotes,
         questions: textQuestions,
       };
     }
-    // if we reach the end
-    if (QOTDData.quoteNumber >= QOTDData.quotes.length) {
-      QOTDData.quoteNumber = 0;
-    }
-    if (QOTDData.questionNumber >= QOTDData.questions.length) {
-      QOTDData.questionNumber = 0;
-    }
+    // If the files have changed, change it.
+    if (QOTDData.quotes !== textQuotes) QOTDData.quotes = textQuotes;
+    if (QOTDData.questions !== textQuestions) QOTDData.questions = textQuestions;
 
     const {
       quoteNumber, questionNumber, quotes, questions,
     } = QOTDData; // destructure
+
+    // find text from random number
     let currentQuote = quotes.filter((quote, index) => quoteNumber === index);
     [currentQuote] = currentQuote;
-
     let currentQuestion = questions.filter(
       (question, index) => questionNumber === index,
     );
     [currentQuestion] = currentQuestion;
 
     // Update QOTD
-    QOTDData.quoteNumber += 1;
-    QOTDData.questionNumber += 1;
+    QOTDData.quoteNumber = Math.floor(Math.random() * quotes.length);
+    QOTDData.questionNumber = Math.floor(Math.random() * questions.length);
+
     // Save data to JSON file
     saveData(QOTDData);
 
