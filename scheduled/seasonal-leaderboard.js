5a20467adadb8c8f90cdde78b5f19fe6f99bf406
David Cao <dacao@protonmail.com>
2022-04-11 19:13:52 -0600

    added some notes

---
+++ scheduled/seasonal-leaderboard.js
@@ -0,0 +1,27 @@
+// If the months are
+// January 1st
+// May 1st
+// September 1st
+//
+//
+//
+// Then increase season by 1
+//
+// --> go into index and add sesaonal leaderboard instead of all-time leaderboard (switch out)
+// all-time leaderboard can be a different command
+//
+// 1. Display seasonal leaderboard
+//  - top 15
+//  - display @ranks as well beside top 5?
+//
+// 2. Cycle through each member and get rid of every role
+//    that contians a grinder in them
+//
+// 3. Add "Slacker" role to everyone
+//
+//
+// 4. Display message in announcements a week before saying how the leaderboard
+//    is going to be reset
+//
+//
+// 5. --> work on making a rnaking system
