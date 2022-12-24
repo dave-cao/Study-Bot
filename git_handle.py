92ef1d8073b2089b232262a72b7f3730a5428d04
David <dacao@protonmail.com>
2022-12-24 11:50:38 -0700

    A day has passed, auto pushed data for backup

---
+++ git_handle.py
@@ -0,0 +1,17 @@
+# automatically pushes all files to github auto
+
+from git import Repo
+
+git_path = "."
+
+def git_push(commit_message):
+    try:
+        repo = Repo(git_path)
+        repo.git.add(all=True)
+        repo.index.commit(commit_message)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print("Some error occured while pushing the code")
+
+git_push("A day has passed, auto pushed data for backup")
